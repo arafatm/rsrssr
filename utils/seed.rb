312efc3f731f2c301f9b8c96fef65cd75c546199
Arafat Mohamed <github@arafatm.com>
2013-03-31 12:01:50 -0500

    seed file to load xkcd and slashdot feeds

--- utils/seed.rb
+++ utils/seed.rb
@@ -1,4 +1,9 @@
 require './app/model/config.rb'
+require './app/backend/reader.rb'
+
+DataMapper.auto_migrate!
 
 Feed.first_or_create(:url => 'http://xkcd.com/rss.xml')
 Feed.first_or_create(:url => 'http://slashdot.org/index.rdf')
+
+Reader.read_feeds
