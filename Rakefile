3dd5b7bfb96be04ee545b839f4723381e818f0c0
Arafat Mohamed <github@arafatm.com>
2013-04-06 14:39:21 -0500

    more observr

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ require 'rake/testtask'
 task :default => [:test_units]
 
 desc "Run basic tests"
-Rake::TestTask.new("test_units") { |t|
+Rake::TestTask.new("tests") { |t|
   t.pattern = 'test/*/*.rb'
   t.verbose = false
   t.warning = false
--- observr.rb
+++ observr.rb
@@ -15,27 +15,54 @@ def watch_app
   end
 end
 
-def run_test(file)
-  unless File.exist?(file)
-    puts "Test File #{file} not created".colorize(:light_red).red
-    return
-  end
-  output = `bundle exec ruby #{file}`
+def parse_test_results(output)
+end
+
+def parse_tests(filename, output)
   result = output.split("\n").grep(/\d+ tests, .*/)
   details = result.to_s.scan(/\d+/)
   fails = details.slice(2..3).any? { |f| f.to_i > 0 }
+  pendings = details.slice(4..6).any? { |f| f.to_i > 0 }
+
+  screen = %Q{screen -X hardstatus alwayslastline }
+  time = Time.now.strftime("%I:%M %p")
+  msg = "#{details[0]}T" +
+    " : #{details[1]}A" +
+    " : #{details[2]}F" +
+    " : #{details[3]}E" +
+    " : #{details[4]}P" +
+    " : #{details[5]}O" +
+    " : #{details[6]}N"
+
   if(fails)
     puts output.colorize(:light_red)
-    system("screen -X hardstatus alwayslastline '%{= rd}Fail ...%='")
+    system %Q{#{screen}'%{= rw}#{time} #{filename}%=#{msg} '}
+  elsif(pendings)
+    puts output.colorize(:light_yellow)
+    system %Q{#{screen}'%{= yw}#{time} #{filename}%=#{msg} '}
   else
-    puts result.to_s.colorize(:light_green)
-    system("screen -X hardstatus alwayslastline '%{= gb}Pass ...%='")
+    puts output.to_s.colorize(:light_green)
+    system %Q{#{screen}'%{= gk}#{time} #{filename}%=#{msg} '}
     run_all
   end
+
+end
+
+def run_test(file)
+  unless File.exist?(file)
+    puts "Test File #{file} not created".colorize(:light_red).red
+    system("screen -X hardstatus alwayslastline '%{b dR} #{file} not created%='")
+    return
+  end
+
+  output = `bundle exec ruby #{file}`
+  parse_tests(file, output)
 end
 
 def run_all
-  system("bundle exec rake")
+  system("echo '\n\n---------------------------------------- Suite'")
+  output = `bundle exec rake tests`
+  parse_tests("ALL", output)
 end
 
 # Ctrl-\
